dd0f87ace9977225b6069eb074a14cc1d91808f7
SinPedro <143435476+SinPedro@users.noreply.github.com>
2023-09-06 16:39:48 -0300

    Add files via upload

---
+++ calculoquadrado/caluloqdr.js
@@ -0,0 +1,7 @@
+const numeroP = prompt("Digite o numero que deseja calcular:");
+const calculoM = numeroP * numeroP
+
+
+
+console.log("Seu número escolhido foi:", numeroP);
+console.log("Seu número multiplicado ao quadrado é:", calculoM);
